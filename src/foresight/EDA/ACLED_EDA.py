b1f35ef1523730daa66f28c74af42afa1ae554d4
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2023-01-28 19:57:42 -0500

    Cleanup

--- src/foresight/EDA/ACLED_EDA.py
+++ src/foresight/EDA/ACLED_EDA.py
@@ -8,27 +8,6 @@ import numpy as np
 pd.options.display.expand_frame_repr = False
 pd.options.display.max_rows = 100
 
-"""
-acled_path = os.path.normpath(r"..\..\..\..\Data\ACLED_012423_CS.csv")
-
-#Note: ACLED Data contains some odd chars that throw errors.
-#This is a working solution for EDA, but we may need to
-#correct the CSV by hand for more stable results
-ACLED = pd.read_csv(acled_path, engine='python', error_bad_lines = False)
-
-
-ACLED['event_date'] = pd.to_datetime(ACLED['event_date'])
-
-labels = ACLED.groupby(
-    [
-        ACLED.country,
-        ACLED.event_date.dt.year.rename('year'),
-        ACLED.event_date.dt.month.rename('month')
-    ]
-    )['fatalities'].sum().reset_index()
-
-"""
-
 #humdata_path = os.path.normpath(os.environ['humdata_path'])
 
 humdata_path = os.path.normpath(r'..\..\..\..\Data\ACLED_via_humdata_012623.xlsx')
@@ -57,32 +36,6 @@ ACLED['Fatalities_Bool'] = ACLED['Fatalities']>1
 
 #ACLED['More_Than_Average'] = ACLED['Fatalities']>
 #ACLED['More_Than_Average_Since_2021'] = ACLED['Fatalities']>
-
-def month_template (start_month, year, n_months):
-    """
-    Returns a DF with years/months for the time period begining
-    1 month before start_month and ending n months before start_month
-    """
-
-    if type(start_month) == str:
-        start_month = month_num[start_month]
-
-    month_years = []
-    month = start_month
-
-    for i in range(n_months):
-        if month <=1:
-            month = 12
-            year = year-1
-        else:
-            month = month-1
-
-        month_years.append((month, year))
-
-    df = pd.DataFrame(month_years)
-    df.rename({0:'Month_Num', 1:'Year'}, axis = 1, inplace=True)
-
-    return df
 
 
 def get_months (start_month, year, n_months):
@@ -127,7 +80,7 @@ def create_base_df(start_month, start_year, country, n_months):
               (ACLED['Country'] == country)
             & (ACLED['Date'] >= daterange[1])
             & (ACLED['Date'] <= daterange[0])
-    ]
+    ].reset_index()
     return df
 
 
