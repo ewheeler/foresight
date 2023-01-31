0cd9b4c2629df54a2c4e0f00e86855ba7f1057e4
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2023-01-30 23:36:06 -0500

    Reimplemented all analysis functions as LabelMaker class. This reduces number of times base_df needs to be computed per row

--- src/foresight/EDA/ACLED_EDA.py
+++ src/foresight/EDA/ACLED_EDA.py
@@ -31,101 +31,124 @@ ACLED['Month_Num'] = ACLED['Month'].map(month_num)
 
 ACLED['Date'] = pd.to_datetime(ACLED['Month'] + ' ' + ACLED['Year'].astype(str))
 
+class LabelMaker():
+    def __init__(self, n_months):
+        self.df = None
+        self.n_months = n_months
+    def get_months(self, start_month, year):
+        n_months = self.n_months
+        if type(start_month) == str:
+            start_month = month_num[start_month]
 
+        month_years = [(year, start_month)]
+        month = start_month
 
-def get_months (start_month, year, n_months, include_start_month = False):
-    """
-    Returns a DF with years/months for the time period begining
-    1 month before start_month and ending n months before start_month
-    """
+        for i in range(n_months):
+            if month <= 1:
+                month = 12
+                year = year - 1
+            else:
+                month = month - 1
 
-    if type(start_month) == str:
-        start_month = month_num[start_month]
+            month_years.append((year, month))
 
-    month_years = []
-    month = start_month
+        start_date = month_years[0]
+        end_date = month_years[-1]
 
-    if include_start_month:
-        month_years.append((year, start_month))
-        n_months = n_months-1
+        date_range = (
+            datetime.datetime(start_date[0], start_date[1], 1)
+            , datetime.datetime(end_date[0], end_date[1], 1)
+        )
 
-    for i in range(n_months):
-        if month <= 1:
-            month = 12
-            year = year - 1
-        else:
-            month = month - 1
+        self.date_range = date_range
+        return date_range
 
-        month_years.append((year, month))
+    def create_base_df(self, country, *args, **kwargs):
+        """
+        Creates a base df of n months from the start date for a given country
+        This can be used for further trend analysis
+        """
+        daterange = self.get_months(*args, **kwargs)
+        df = ACLED[
+            (ACLED['Country'].values == country)
+            & (ACLED['Date'].values >= np.datetime64(daterange[1]))
+            & (ACLED['Date'].values <= np.datetime64(daterange[0]))
+            ].reset_index()
+        self.base_df = df
 
-    start_date = month_years[0]
-    end_date = month_years[-1]
+    def fatalities_previous_month(self):
+        """
+        Returns Fatalities for preceding month
+        """
+        if len(self.base_df) <= 1:
+            return None
+        return self.base_df['Fatalities'].iloc[-2]
 
-    date_range = (
-         datetime.datetime(start_date[0], start_date[1], 1)
-        ,datetime.datetime(end_date[0], end_date[1], 1)
-    )
+    def n_month_mean(self):
+        """
+        returns average for the n months preceding the current month
+        Does not include current month
+        """
 
-    return date_range
+        return self.base_df['Fatalities'].iloc[0:-1].mean()
 
-def create_base_df(country, *args, **kwargs):
-    """
-    Creates a base df of n months from the start date for a given country
-    This can be used for further trend analysis
-    """
-    daterange = get_months(*args, **kwargs)
+    def n_month_std(self):
+        """
+        returns average for the n months preceding the current month
+        Does not include current month
+        """
 
-    df = ACLED[
-             (ACLED['Country'].values == country)
-           & (ACLED['Date'].values >= np.datetime64(daterange[1]))
-           & (ACLED['Date'].values <= np.datetime64(daterange[0]))
-    ].reset_index()
-    return df
+        return self.base_df['Fatalities'].iloc[0:-1].std()
+    def n_month_trend(self):
+        """
+        Calculates trend for the last n months
+        Does include current month
+        """
 
+        y = self.base_df.iloc[-n_months:]['Fatalities']
+        x = np.array(range(len(y))).reshape(-1, 1)
 
-def fatalities_previous_month(country, start_month, start_year):
-    base_df = create_base_df(country, start_month, start_year, 1)
-    if len(base_df) == 0:
-        return None
-    return base_df['Fatalities'].iloc[0]
+        model = LinearRegression()
+        model.fit(x, y)
+        slope = model.coef_
+        return slope[0]
 
+    def apply_by_row(self, row):
+        self.create_base_df(row['Country'], row['Month'], row['Year'])
+        prev = self.fatalities_previous_month()
+        mean = self.n_month_mean()
+        std = self.n_month_std()
+        trend = self.n_month_trend()
 
-def n_month_mean(country, *args, **kwargs):
-    base_df = create_base_df(country, *args, **kwargs)
-    return base_df['Fatalities'].mean()
-
-
-def n_month_trend(country, *args, **kwargs):
-    kwargs.update({'include_start_month':True})
-    base_df = create_base_df(country, *args, **kwargs)
-    x = base_df.index.values.reshape(-1,1)
-    y = base_df['Fatalities']
-    model = LinearRegression()
-    model.fit(x,y)
-    slope = model.coef_
-    return slope[0]
-
-
-
-def apply_by_row(row, func, **kwargs):
-    return func(row['Country'], row['Month'], row['Year'], **kwargs)
+        return(prev, mean, std, trend)
 
 """
 START ADDING STUFF
 """
 
-n_months
+n_months = 3
+
+Labler = LabelMaker(n_months=3)
+
+ACLED = pd.read_excel(humdata_path, sheet_name='Data')
+
+ACLED['Month_Num'] = ACLED['Month'].map(month_num)
+
+ACLED['Date'] = pd.to_datetime(ACLED['Month'] + ' ' + ACLED['Year'].astype(str))
 
 #Scalers
-ACLED['Fatalities_Last_Month'] = ACLED.apply(apply_by_row, args = [fatalities_previous_month], axis = 1)
-ACLED['Mean_Last_3_Months'] = ACLED.apply(apply_by_row, args = [n_month_mean], n_months=n_months, axis = 1)
-ACLED['Trend'] = ACLED.apply(apply_by_row, args = [n_month_trend], n_months=n_months, axis = 1)
+ACLED[['Fatalities_Last_Month',
+       f'Mean_Last_{n_months}_Months',
+       f'Last_{n_months}_Month_STD',
+       'Trend']] = ACLED.apply(Labler.apply_by_row, result_type='expand', axis = 1)
+
 ACLED['Theta'] = np.arctan(ACLED['Trend'])
+ACLED['Deviation_From_Mean'] = ACLED['Fatalities'] - ACLED[f'Mean_Last_{n_months}_Months']
 
 #Bools
 ACLED['Fatalities_Bool'] = ACLED['Fatalities']>0
 ACLED['More_Than_Last_month'] = ACLED['Fatalities']>ACLED['Fatalities_Last_Month']
-ACLED['More_Than_Average'] = ACLED['Fatalities']>ACLED['Mean_Last_3_Months']
+ACLED['More_Than_Average'] = ACLED['Fatalities']>ACLED[f'Mean_Last_{n_months}_Months']
+ACLED[f'Outside_{n_months}_Month_Std'] = ACLED['Deviation_From_Mean'] > ACLED[ f'Last_{n_months}_Month_STD']
 ACLED['Trend_Increasing'] = ACLED['Trend']>0
-
 
