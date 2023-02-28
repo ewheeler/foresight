01ed6e5ec0d2bbb2b154a8112e7737926174fbc9
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2023-02-28 16:36:17 -0500

    more helper funcs

--- src/foresight/Training_Pipeline/TFRecord_pipeline.py
+++ src/foresight/Training_Pipeline/TFRecord_pipeline.py
@@ -17,7 +17,8 @@ y_var = 'Trend_Increasing' #Column in ACLED to use as y
 n_months = 1 # number of months to sample for features
 lag_time = 2 # months between last feature month and label month
 
-gdelt_dir = 'gcs://frsght/datasets_sample_b988c8_694a90_1f5902/gdelt' #WHERE TO GET FEATURES
+#gdelt_dir = 'datasets_sample_b988c8_694a90_1f5902/gdelt' #WHERE TO GET FEATURES
+gdelt_dir = 'datasets_stacked/gdelt'
 tfrecord_dir = 'gcs://frsght/datasets_stacked/tfrecords_1' #WHERE TO SAVE OUTPUT
 
 ###Setting up Bucket####
@@ -25,8 +26,8 @@ GCP_project = 'foresight-375620'
 GCPClient = storage.Client(project=GCP_project)
 bucket = GCPClient.bucket('frsght')
 
-blobs = [b.name for b in bucket.list_blobs(prefix='datasets_sample_b988c8_694a90_1f5902/gdelt')]
-blobs = [b for b in blobs if b.endswith('parquet')]
+blobs = [b.name for b in bucket.list_blobs(prefix=gdelt_dir)]
+#blobs = [b for b in blobs if b.endswith('parquet')]
 blobs = [f'gcs://frsght/{b}' for b in blobs]
 
 ###reading in ACLED labels
@@ -84,25 +85,71 @@ def read_in_month(yearmonth, verbose = 1):
     return df
 
 
+def read_in_country_data(yearmonths, country,  verbose = 0):
+    files = [b for b in blobs if any(ym in b for ym in yearmonths) and country in b]
+    df = pd.DataFrame()
+    prog = 0
+    if len(files) > 0:
+        for file in files:
+            df = pd.concat([df, pd.read_parquet(file)])
+            if verbose == 1:
+                fprog = fprog + 1
+                prog_bar(fprog, len(files))
+
+    return df
+
+
 def parse_gdelt_data(df, n):
     #todo implement better sequence embeddings
     df = df.copy()
+
     #df['date_delta'] = (pd.to_datetime(df['date']) - target_date).dt.days
     #df['date_delta'] = df['date_delta']/norm_days
 
    # embeddings = df[embedding_cols + ['date_delta']]
-
     n_samples = int(np.floor(len(df)/n))
+    if n_samples >0:
+        embeddings = df.sample(len(df))
+        embeddings = [embeddings.iloc[n * i:n * i + n].sort_values('date')[embedding_cols].values for i in range(n_samples)]
+    else:
+        embeddings = df.sort_values('date')[embedding_cols].values
+        padding = n - embeddings.shape[0]
+        embeddings = np.pad(embeddings, [(0, padding), (0, 0)])
 
-    embeddings = df.sample(len(df))
     #returns list of nx512 np arrays. Each array is sorted by date within itself, but each array consists of a random sample of articles
-    return[embeddings.iloc[n*i:n*i+n].sort_values('date')[embedding_cols].values for i in range(n_samples)]
+    return embeddings
 
 def get_label(yearmonth, country, label_col):
     label = ACLED[(ACLED['yearmonth'] == yearmonth) & (ACLED['Code'] == country)][label_col].iloc[0]
     return label
 
-def create_feature_dict(df, yearmonth, country, label_col, n, )
+def parse_yearmonths(start, n_months):
+    start_year = int(start[0:4])
+    start_month = int(start[4:6])
+    yearmonths = [start]
+    year = start_year
+    month = start_month
+    for i in range(n_months):
+        if month <= 1:
+            month = 12
+            year = year - 1
+        else:
+            month = month - 1
+
+        write_year = str(year)
+        write_month = str(month).rjust(2,'0')
+        ym = write_year+write_month
+        yearmonths.append(ym)
+
+    return yearmonths
+
+
+
+
+
+def build_example_dict(country, target_yearmonth):
+    pass
+
 
 ###Main loop###
 ##for ym in yearmonths[0:1]:
